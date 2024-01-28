ea5956956aba4438aefb6e538cd4c3782601d3f8
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2024-01-28 22:26:29 +0100

    Fix header offset

--- src/components/Cell/HeadCell.tsx
+++ src/components/Cell/HeadCell.tsx
@@ -1,12 +1,12 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material"
 
 interface HeadCellProps {
-  style: React.CSSProperties;
-  columnIndex: number;
-  rowIndex: number;
-  hourRange: string[];
-  dateRange: string[];
-  weekRange: string[];
+  style: React.CSSProperties
+  columnIndex: number
+  rowIndex: number
+  hourRange: string[]
+  dateRange: string[]
+  weekRange: string[]
 }
 
 export function HeadCell({
@@ -18,20 +18,19 @@ export function HeadCell({
   weekRange,
 }: HeadCellProps) {
   const renderLabel = (range: string[], columnIndex: number, span: number) => {
-    const index = columnIndex % (range.length * span);
-    const label = range[Math.floor(index / span)];
+    const index = columnIndex % (range.length * span)
+    const label = range[Math.floor(index / span)]
     if ((columnIndex % span) - 1 == 0) {
-      return <Typography paddingLeft="1rem">{label}</Typography>;
+      return <Typography paddingLeft="1rem">{label}</Typography>
     } else {
-      return <Typography sx={{ color: "transparent" }}>/</Typography>;
+      return <Typography sx={{ color: "transparent" }}>/</Typography>
     }
-  };
+  }
 
   return (
     <div
       style={{
         ...style,
-        backgroundColor: "white",
       }}
     >
       <Stack
@@ -39,12 +38,14 @@ export function HeadCell({
           width: "100%",
           height: 100,
           bgcolor: "#D9D9D9",
+          boxSizing: "border-box",
         }}
       >
         <Stack
           height="100%"
           justifyContent="center"
           sx={{
+            boxSizing: "content-box",
             borderBottom: columnIndex != 0 ? "1px solid black" : "",
             //display right border every 7 * 24 * 4th column
             borderRight:
@@ -53,17 +54,23 @@ export function HeadCell({
         >
           {renderLabel(weekRange, columnIndex, 24 * 7 * 4)}
         </Stack>
-        <Stack
+        <Box
           height="100%"
-          justifyContent="center"
           sx={{
-            borderBottom: columnIndex != 0 ? "1px solid black" : "",
             //display right border every 24 * 4th column
             borderRight: columnIndex % (24 * 4) == 0 ? "1px solid black" : "",
           }}
         >
-          {renderLabel(dateRange, columnIndex, 24 * 4)}
-        </Stack>
+          <Stack
+            justifyContent="center"
+            style={{
+              borderBottom: columnIndex != 0 ? "1px solid black" : "",
+              height: "100%",
+            }}
+          >
+            {renderLabel(dateRange, columnIndex, 24 * 4)}
+          </Stack>
+        </Box>
         <Stack
           height="100%"
           justifyContent="center"
@@ -80,5 +87,5 @@ export function HeadCell({
         </Stack>
       </Stack>
     </div>
-  );
+  )
 }
